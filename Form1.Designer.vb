9c1943c6f3e1fd14e56cd1e91682a887b0b9d8e7
Bhavya30823 <60810721+Bhavya30823@users.noreply.github.com>
2020-02-29 23:39:23 +0530

    first commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SHOW = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.SHOW, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SHOW
+        '
+        Me.SHOW.Location = New System.Drawing.Point(12, 22)
+        Me.SHOW.Name = "SHOW"
+        Me.SHOW.Size = New System.Drawing.Size(776, 393)
+        Me.SHOW.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.SHOW.TabIndex = 0
+        Me.SHOW.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(58, 421)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "show"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(243, 421)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "next"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(403, 421)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "prev"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.SHOW)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.SHOW, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SHOW As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,48 @@
+﻿
+Public Class Form1
+    Dim a As Integer = 0
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim gallery(4) As String
+        gallery = New String() {"F:\UDAIPUR\IMG_20191220_050124.jpg", "F:\UDAIPUR\IMG_20191220_050131.jpg", "F:\UDAIPUR\IMG_20191220_064200.jpg", "F:\UDAIPUR\IMG_20191220_064203.jpg"}
+        Dim strfilename As String = gallery(0)
+        Dim bhavya As Image
+        bhavya = Image.FromFile(strfilename)
+        SHOW.Image = bhavya
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        a = a + 1
+        If a = 4 Then
+            MessageBox.Show("khatam")
+            End
+        End If
+        Dim gallery(4) As String
+        gallery = New String() {"F:\UDAIPUR\IMG_20191220_050124.jpg", "F:\UDAIPUR\IMG_20191220_050131.jpg", "F:\UDAIPUR\IMG_20191220_064200.jpg", "F:\UDAIPUR\IMG_20191220_064203.jpg"}
+        Dim strfilename As String = gallery(a)
+        Dim bhavya As Image
+        bhavya = Image.FromFile(strfilename)
+        SHOW.Image = bhavya
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim gallery(4) As String
+        gallery = New String() {"F:\UDAIPUR\IMG_20191220_050124.jpg", "F:\UDAIPUR\IMG_20191220_050131.jpg", "F:\UDAIPUR\IMG_20191220_064200.jpg", "F:\UDAIPUR\IMG_20191220_064203.jpg"}
+        If a = 0 Then
+            MessageBox.Show("you are on the 1st picture")
+            a = a + 1
+        End If
+        a = a - 1
+        Dim strfilename As String = gallery(a)
+        Dim bhavya As Image
+        bhavya = Image.FromFile(strfilename)
+        SHOW.Image = bhavya
+
+    End Sub
+End Class
